5a51ee4c16f122b08e7bc6dbb98403764adc36d9
Saul Nunez <saul.nunez99@gmail.com>
2025-08-18 09:22:43 -0600

    Connect with database

--- CrowdSage.Server/Program.cs
+++ CrowdSage.Server/Program.cs
@@ -1,6 +1,12 @@
+using CrowdSage.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<CrowdsageDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
