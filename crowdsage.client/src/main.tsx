5060ced7ec088220e5338fe55279d166bbbabca0
Saul Nunez <saul.nunez99@gmail.com>
2025-10-02 00:42:51 -0600

    Add React Router

--- crowdsage.client/src/main.tsx
+++ crowdsage.client/src/main.tsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import QuestionPage from './Screens/QuestionPage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    children: [
+      {
+        path: "question/:questionId",
+        Component: QuestionPage
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
