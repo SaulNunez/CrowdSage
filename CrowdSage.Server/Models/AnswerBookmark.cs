f54044b48d9bf5552168285ec345af5e99c2f6c0
Saul Nunez <saul.nunez99@gmail.com>
2025-11-21 19:53:58 -0700

    Fix models to use CrowdSageUser rather than IdentityUser

--- CrowdSage.Server/Models/AnswerBookmark.cs
+++ CrowdSage.Server/Models/AnswerBookmark.cs
@@ -8,5 +8,5 @@ public class AnswerBookmark
     public Guid AnswerId { get; set; }
     public Answer Answer { get; set; }
     public string UserId { get; set; }
-    public IdentityUser User { get; set; }
+    public CrowdsageUser User { get; set; }
 }
--- CrowdSage.Server/Models/QuestionBookmark.cs
+++ CrowdSage.Server/Models/QuestionBookmark.cs
@@ -8,5 +8,5 @@ public class QuestionBookmark
     public Guid QuestionId { get; set; }
     public Question Question { get; set; }
     public string UserId { get; set; }
-    public IdentityUser User { get; set; }
+    public CrowdsageUser User { get; set; }
 }
