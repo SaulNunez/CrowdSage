d79bb3eaa4c24c59f491b127ad47e88069bf9497
Saul Nunez <saul.nunez99@gmail.com>
2026-03-25 23:14:20 -0600

    Setup translations in spanish and english

--- crowdsage.client/src/main.tsx
+++ crowdsage.client/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './i18n'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import QuestionPage from './Screens/QuestionPage.tsx'
